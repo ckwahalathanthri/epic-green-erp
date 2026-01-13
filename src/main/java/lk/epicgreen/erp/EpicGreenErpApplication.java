2606ab4eb46af9d46d960a493188590c70cfb9db
Muhammed Ali Saifudeen <saifudeen_m@epic.lk>
2026-01-12 21:19:55 -0800

    Fixed sql error

--- src/main/java/lk/epicgreen/erp/EpicGreenErpApplication.java
+++ src/main/java/lk/epicgreen/erp/EpicGreenErpApplication.java
@@ -2,11 +2,16 @@ package lk.epicgreen.erp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+
+
 @SpringBootApplication
-@EnableJpaRepositories("lk.epicgreen.erp.*.repository")
+//@EnableJpaRepositories("lk.epicgreen.erp.*.repository")
+//@EntityScan("lk.epicgreen.erp")
+//@EnableJpaRepositories("lk.epicgreen.erp")
 public class EpicGreenErpApplication {
 
 	public static void main(String[] args) {
