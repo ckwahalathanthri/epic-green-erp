653855f2b730c9f6d97ccddec214d13ca78747f2
JAlavindu <lavi156perera@gmail.com>
2026-01-26 08:32:05 +0530

    oauth login

--- src/main/java/lk/epicgreen/erp/admin/config/SecurityConfig.java
+++ src/main/java/lk/epicgreen/erp/admin/config/SecurityConfig.java
@@ -61,7 +61,6 @@ public class SecurityConfig {
             .authenticationProvider(authenticationProvider());
             //.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
             
-
         return http.build();
     }
 
