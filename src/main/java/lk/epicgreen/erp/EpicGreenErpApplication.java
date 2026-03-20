ee7d8f3d098735ee03d377fd780639b7cc966584
Muhammed Ali Saifudeen <saifudeen_m@epic.lk>
2026-03-19 20:38:49 -0700

    alterations

--- src/main/java/lk/epicgreen/erp/EpicGreenErpApplication.java
+++ src/main/java/lk/epicgreen/erp/EpicGreenErpApplication.java
@@ -9,9 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication(scanBasePackages = "lk.epicgreen.erp")
 @EnableJpaRepositories(basePackages = "lk.epicgreen.erp")
 @EntityScan(basePackages = "lk.epicgreen.erp")
-public class EpicGreenErpApplication {
-
-	public static void main(String[] args) {
+public class EpicGreenErpApplication { public static void main(String[] args) {
 		SpringApplication.run(EpicGreenErpApplication.class, args);
 	}
 
